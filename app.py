e375cc71e255ef246582ce509a6994f1b2f567c0
anamvarela <100178801+anamvarela@users.noreply.github.com>
2025-07-09 18:22:54 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -32,10 +32,10 @@ with open('config.yaml') as file:
 
 # Criar o autenticador
 authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days']
+    credentials=config['credentials'],
+    cookie_name=config['cookie']['name'],
+    key=config['cookie']['key'],
+    cookie_expiry_days=config['cookie']['expiry_days']
 )
 
 # Adicionar login
